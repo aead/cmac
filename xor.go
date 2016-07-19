3ff573cc63b88247f6883142d37314dc9add2f08
Andreas Auernhammer <aead@mail.de>
2016-07-19 13:52:27 +0200

    Add CMAC and AES-CMAC implementation

---
+++ xor.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2016 Andreas Auernhammer. All rights reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+// +build !amd64
+
+package cmac
+
+// XOR xors the bytes in src and with and writes the result to dst.
+// The destination is assumed to have enough space. Returns the
+// number of bytes xor'd.
+func XOR(dst, src, with []byte) int {
+	var a, b []byte
+	if len(src) <= len(with) {
+		a = src
+		b = with
+	} else {
+		b = src
+		a = with
+	}
+
+	for i, v := range a {
+		dst[i] = b[i] ^ v
+	}
+	return len(a)
+}
---
+++ xor_amd64.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2016 Andreas Auernhammer. All rights reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+// +build amd64, !gccgo, !appengine
+
+package cmac
+
+import "unsafe"
+
+const wordSize = int(unsafe.Sizeof(uintptr(0)))
+
+// XOR xors the bytes in src and with and writes the result to dst.
+// The destination is assumed to have enough space. Returns the
+// number of bytes xor'd.
+func xor(dst, src, with []byte) int {
+	n := len(src)
+	if len(with) < n {
+		n = len(with)
+	}
+
+	w := n / wordSize
+	if w > 0 {
+		dstPtr := *(*[]uintptr)(unsafe.Pointer(&dst))
+		srcPtr := *(*[]uintptr)(unsafe.Pointer(&src))
+		withPtr := *(*[]uintptr)(unsafe.Pointer(&with))
+		for i, v := range srcPtr[:w] {
+			dstPtr[i] = withPtr[i] ^ v
+		}
+	}
+
+	for i := (n & (^(wordSize - 1))); i < n; i++ {
+		dst[i] = src[i] ^ with[i]
+	}
+
+	return n
+}
